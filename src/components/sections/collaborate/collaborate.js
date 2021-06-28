570c2ec596d069f17c1f5603319e7625153abcc9
Luis Faria <luisrifaria@gmail.com>
2021-06-28 12:42:59 +0100

    set max width and margin zero auto to content of the page

--- src/components/sections/collaborate/collaborate.js
+++ src/components/sections/collaborate/collaborate.js
@@ -3,48 +3,50 @@ import './collaborate.scss';
 
 const collaborate = () => {
   return (
-    <div className='collab-wrapper'>
-        <div className="title-description">
-            <h2>COLLABORATE</h2>
-            <h3>WITH US</h3>
-            <article>
-                <p>Lorem ipsum dolor sit amet, consecetur adipiscing elit, sed do eiumod tempo incididunt ut labore et dolore magna aliqua.</p><br></br>
-                <p className='second-p'>Ut enim ad minim venlam, quis nostrud exercitation ullamco laboris nisi utaliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in coluptate velit esse cillum dolore.</p>
-            </article>
+      <div className='collab-wrapper'>
+        <div className='center-content'>
+            <div className="title-description">
+                <h2>COLLABORATE</h2>
+                <h3>WITH US</h3>
+                <article>
+                    <p>Lorem ipsum dolor sit amet, consecetur adipiscing elit, sed do eiumod tempo incididunt ut labore et dolore magna aliqua.</p><br></br>
+                    <p className='second-p'>Ut enim ad minim venlam, quis nostrud exercitation ullamco laboris nisi utaliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in coluptate velit esse cillum dolore.</p>
+                </article>
+            </div>
+
+            <div className='form-wrapper'>
+                <form className='form'>
+                    <div className='form-left'>
+                        <label>NAME</label>
+                        <input type='text' className='form-field' placeholder='Name'></input>
+
+                        <label>PHONE</label>
+                        <input type='text' className='form-field' placeholder='Phone'></input>
+
+                        <label>EXPERIENCE</label>
+                        <select id="experience" className='select-field' name="experience">
+                            <option value="some-value">Select</option>
+                        </select>
+                    </div>
+                    <div className='form-right'>
+                        <label>SURNAME</label>
+                        <input type='text' className='form-field' placeholder='Surname'></input>
+
+                        <label>EMAIL ADDRESS</label>
+                        <input type='email' className='form-field' placeholder='Email'></input>
+
+                        <label>WHERE DID YOU HEAR ABOUT US?</label>
+                        <select id="social" className='select-field' name="social">
+                            <option value="some-value">Select</option>
+                        </select>
+                        <button className='button-send'>SEND</button>
+                    </div>
+                
+                </form>
+
+            </div>
         </div>
-
-        <div className='form-wrapper'>
-            <form className='form'>
-                <div className='form-left'>
-                    <label>NAME</label>
-                    <input type='text' className='form-field' placeholder='Name'></input>
-
-                    <label>PHONE</label>
-                    <input type='text' className='form-field' placeholder='Phone'></input>
-
-                    <label>EXPERIENCE</label>
-                    <select id="experience" className='select-field' name="experience">
-                        <option value="some-value">Select</option>
-                    </select>
-                </div>
-                <div className='form-right'>
-                    <label>SURNAME</label>
-                    <input type='text' className='form-field' placeholder='Surname'></input>
-
-                    <label>EMAIL ADDRESS</label>
-                    <input type='email' className='form-field' placeholder='Email'></input>
-
-                    <label>WHERE DID YOU HEAR ABOUT US?</label>
-                    <select id="social" className='select-field' name="social">
-                        <option value="some-value">Select</option>
-                    </select>
-                    <button className='button-send'>SEND</button>
-                </div>
-               
-            </form>
-
-        </div>
-    </div>
+      </div>
   );
 }
 
--- src/components/sections/footer/footer.js
+++ src/components/sections/footer/footer.js
@@ -4,46 +4,50 @@ import './footer.scss';
 const footer = () => {
   return (
     <div className='footer-wrapper'>
-        <div className='sections'>
-            <div className='single-section'>
-                <h4>SECTION 1</h4>
-                <p>Subsection</p>
-                <p>Subsection</p>
-                <p>Subsection</p>
-                <p>Subsection</p>
+        <div className='center-content'>
+            <div className='sections'>
+                <div className='single-section'>
+                    <h4>SECTION 1</h4>
+                    <p>Subsection</p>
+                    <p>Subsection</p>
+                    <p>Subsection</p>
+                    <p>Subsection</p>
+                </div>
+                <div className='single-section'>
+                    <h4>SECTION 2</h4>
+                    <p>Subsection</p>
+                    <p>Subsection</p>
+                    <p>Subsection</p>
+                    <p>Subsection</p>
+                </div>
+                <div className='single-section'>
+                    <h4>SECTION 3</h4>
+                    <p>Subsection</p>
+                    <p>Subsection</p>
+                    <p>Subsection</p>
+                    <p>Subsection</p>
+                </div>
+                <div className='single-section'>
+                    <h4>SECTION 4</h4>
+                    <p>Subsection</p>
+                    <p>Subsection</p>
+                    <p>Subsection</p>
+                    <p>Subsection</p>
+                </div>
+                <div className='single-section'>
+                    <h4>SECTION 5</h4>
+                    <p>Subsection</p>
+                    <p>Subsection</p>
+                    <p>Subsection</p>
+                    <p>Subsection</p>
+                </div>
             </div>
-            <div className='single-section'>
-                <h4>SECTION 2</h4>
-                <p>Subsection</p>
-                <p>Subsection</p>
-                <p>Subsection</p>
-                <p>Subsection</p>
+
+            <div className='logo-footer'>
+                <h2>This is the</h2>
+                <h2>LOGO</h2>
             </div>
-            <div className='single-section'>
-                <h4>SECTION 3</h4>
-                <p>Subsection</p>
-                <p>Subsection</p>
-                <p>Subsection</p>
-                <p>Subsection</p>
-            </div>
-            <div className='single-section'>
-                <h4>SECTION 4</h4>
-                <p>Subsection</p>
-                <p>Subsection</p>
-                <p>Subsection</p>
-                <p>Subsection</p>
-            </div>
-            <div className='single-section'>
-                <h4>SECTION 5</h4>
-                <p>Subsection</p>
-                <p>Subsection</p>
-                <p>Subsection</p>
-                <p>Subsection</p>
-            </div>
-        </div>
-        <div className='logo-footer'>
-            <h2>This is the</h2>
-            <h2>LOGO</h2>
+            
         </div>
     </div>
   );
